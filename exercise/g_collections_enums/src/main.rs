56b1be0f0727cc323fd58d54fd14dfaa0b228b10
Jakob Arsement <jakobarsement@gmail.com>
2024-02-25 17:44:01 -0600

    Loop through a vector and call a member method of each vector element

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -33,55 +33,61 @@ impl Shot {
 }
 
 fn main() {
-    // // Simulate shooting a bunch of arrows and gathering their coordinates on the target.
-    // let arrow_coords: Vec<Coord> = get_arrow_coords(5);
-    // let mut shots: Vec<Shot> = Vec::new();
+    // Simulate shooting a bunch of arrows and gathering their coordinates on the target.
+    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
+    let mut shots: Vec<Shot> = Vec::new();
 
-    // // 2. For each coord in arrow_coords:
-    // //
-    // //   A. Call `coord.print_description()`
-    // //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
-    // //   `coord.distance_from_center()`
-    // //      - Less than 1.0 -- `Shot::Bullseye`
-    // //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
-    // //      - Greater than 5.0 -- `Shot::Miss`
+    // 2. For each coord in arrow_coords:
+    //
+    //   A. Call `coord.print_description()`
+    //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
+    //   `coord.distance_from_center()`
+    //      - Less than 1.0 -- `Shot::Bullseye`
+    //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
+    //      - Greater than 5.0 -- `Shot::Miss`
 
-    // let mut total = 0;
-    // // 3. Finally, loop through each shot in shots and add its points to total
+    let amount_of_shots = arrow_coords.len();
 
-    // println!("Final point total is: {}", total);
+    for i in 0..amount_of_shots {
+        println!("{:?}", arrow_coords[i].print_description());
+    }
+
+    let mut total = 0;
+    // 3. Finally, loop through each shot in shots and add its points to total
+
+    println!("Final point total is: {}", total);
 }
 
-// // A coordinate of where an Arrow hit
-// #[derive(Debug)]
-// struct Coord {
-//     x: f64,
-//     y: f64,
-// }
+// A coordinate of where an Arrow hit
+#[derive(Debug)]
+struct Coord {
+    x: f64,
+    y: f64,
+}
 
-// impl Coord {
-//     fn distance_from_center(&self) -> f64 {
-//         (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
-//     }
-//     fn print_description(&self) {
-//         println!(
-//             "coord is {:.1} away, at ({:.1}, {:.1})",
-//             self.distance_from_center(),
-//             self.x,
-//             self.y
-//         );
-//     }
-// }
+impl Coord {
+    pub fn distance_from_center(&self) -> f64 {
+        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
+    }
+    fn print_description(&self) {
+        println!(
+            "coord is {:.1} away, at ({:.1}, {:.1})",
+            self.distance_from_center(),
+            self.x,
+            self.y
+        );
+    }
+}
 
-// // Generate some random coordinates
-// fn get_arrow_coords(num: u32) -> Vec<Coord> {
-//     let mut coords: Vec<Coord> = Vec::new();
-//     for _ in 0..num {
-//         let coord = Coord {
-//             x: (rand::random::<f64>() - 0.5) * 12.0,
-//             y: (rand::random::<f64>() - 0.5) * 12.0,
-//         };
-//         coords.push(coord);
-//     }
-//     coords
-// }
+// Generate some random coordinates
+fn get_arrow_coords(num: u32) -> Vec<Coord> {
+    let mut coords: Vec<Coord> = Vec::new();
+    for _ in 0..num {
+        let coord = Coord {
+            x: (rand::random::<f64>() - 0.5) * 12.0,
+            y: (rand::random::<f64>() - 0.5) * 12.0,
+        };
+        coords.push(coord);
+    }
+    coords
+}
